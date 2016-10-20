58e289f1c7262bd4ee78fd8f1b2def2bdbf40f14
wangweihua <wangweihua2@huawei.com>
2016-10-20 11:47:37 +0800

    change the way to get  url and login information of database in nslcm
    we need to ensure that we can change the url and login information of database that only need to change a file
    
    SDNO-740
    Signed-off-by: wangweihua <wangweihua2@huawei.com>
    Change-Id: I7d0e6eeca2e6922b9eaca32970966f6cc35c8160

--- service/src/main/java/org/openo/sdno/nslcm/dao/multi/MultiDataSource.java
+++ service/src/main/java/org/openo/sdno/nslcm/dao/multi/MultiDataSource.java
@@ -16,7 +16,21 @@
 
 package org.openo.sdno.nslcm.dao.multi;
 
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.openo.baseservice.remoteservice.exception.ServiceException;
+import org.openo.sdno.nslcm.util.Const;
+import org.openo.sdno.nslcm.util.exception.ApplicationException;
+import org.openo.sdno.overlayvpn.consts.HttpCode;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 /**
  * Class for routing data source.<br>
@@ -28,15 +42,61 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  */
 public class MultiDataSource extends AbstractRoutingDataSource {
 
-    /**
-     * Get current data source key.<br>
-     * 
-     * @return source key
-     * @since SDNO 0.5
-     */
+    private Map<Object, Object> targetDataSources = new HashMap<Object, Object>();
+
+    private static final String CONFIG_PATH = "generalconfig/jdbc.json";
+
     @Override
     protected Object determineCurrentLookupKey() {
-        return DataSourceHolder.getDataSource();
+        String dataSourceName = DataSourceHolder.getDataSource();
+        if(dataSourceName == null) {
+            dataSourceName = "inventory";
+        }
+        try {
+            this.setDataSource(dataSourceName);
+        } catch(ServiceException e) {
+            dataSourceName = "inventory";
+        }
+        return dataSourceName;
     }
 
+    private void setDataSource(String dataSourceName) throws ServiceException {
+        Map<String, String> values = getJsonDataFromFile(CONFIG_PATH);
+        String driverClassName = values.get("driverClassName");
+        String url = values.get("jdbcUrl");
+        String userName = values.get("jdbcUsername");
+        String password = values.get("jdbcPassword");
+        ComboPooledDataSource dataSource = this.createDataSource(driverClassName, url, userName, password);
+        this.addTargetDataSource(dataSourceName, dataSource);
+    }
+
+    private ComboPooledDataSource createDataSource(String driverClassName, String url, String username,
+            String password) {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass(driverClassName);
+            dataSource.setJdbcUrl(url);
+            dataSource.setUser(username);
+            dataSource.setPassword(password);
+        } catch(PropertyVetoException e) {
+            throw new ApplicationException(HttpCode.ERR_FAILED, Const.OPER_DB_FAIL);
+        }
+        return dataSource;
+    }
+
+    private void addTargetDataSource(String dataSourceName, ComboPooledDataSource dataSource) {
+        this.targetDataSources.put(dataSourceName, dataSource);
+        super.setTargetDataSources(this.targetDataSources);
+        afterPropertiesSet();
+    }
+
+    private static Map<String, String> getJsonDataFromFile(String path) throws ServiceException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            byte[] bytes = Files.readAllBytes(Paths.get(path));
+            return mapper.readValue(bytes, Map.class);
+        } catch(IOException e) {
+            throw new ApplicationException(HttpCode.ERR_FAILED, Const.OPER_DB_FAIL);
+        }
+    }
 }
